e3a5b9a1b3014f0dc91cbbd6d17f8c9b2585c542
Diego Marulanda B. <56690521+DiegoMarulandaB@users.noreply.github.com>
2024-03-16 21:05:28 -0500

    refactor: :bug: add fix_import

--- pages/api/faq.ts/faq.tsx
+++ pages/api/faq.ts/faq.tsx
@@ -1,10 +1,10 @@
-import { type NextApiRequest, type NextApiResponse } from 'next'
-import { faqsData } from 'dh-marvel/components/faqs/faqsData'
+import { type NextApiRequest, type NextApiResponse } from 'next';
+import { faqsData } from 'dh-marvel/components/faqs/faqsData';
 // import { faqsData } from '../../../components/faqs/faqsData'
-export default function handler (req: NextApiRequest, res: NextApiResponse) {
+export default function handler(req: NextApiRequest, res: NextApiResponse) {
   if (req.method === 'GET') {
-    res.status(200).json(faqsData)
+    res.status(200).json(faqsData);
   } else {
-    res.status(400).json({ error: 'Método no permitido' })
+    res.status(400).json({ error: 'Método no permitido' });
   }
 }
