d726d490debaa71a42d43843a53c5efaab1bfc98
Diego Marulanda B. <56690521+DiegoMarulandaB@users.noreply.github.com>
2024-03-16 21:04:35 -0500

    refactor: :bug: add fix import

--- pages/api/faq.ts/faq.tsx
+++ pages/api/faq.ts/faq.tsx
@@ -1,6 +1,6 @@
 import { type NextApiRequest, type NextApiResponse } from 'next'
-// import { faqsData } from 'dh-marvel/components/faqs/faqsData'
-import { faqsData } from '../../../components/faqs/faqsData'
+import { faqsData } from 'dh-marvel/components/faqs/faqsData'
+// import { faqsData } from '../../../components/faqs/faqsData'
 export default function handler (req: NextApiRequest, res: NextApiResponse) {
   if (req.method === 'GET') {
     res.status(200).json(faqsData)
