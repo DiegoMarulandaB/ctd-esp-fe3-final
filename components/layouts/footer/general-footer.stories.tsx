865cf87b2ff0820b5c4eaffb2846c0c4bfe4210d
Diego Marulanda B. <56690521+DiegoMarulandaB@users.noreply.github.com>
2024-03-19 09:59:06 -0500

    refactor: :bug: add fixes

--- components/layouts/footer/general-footer.stories.tsx
+++ components/layouts/footer/general-footer.stories.tsx
@@ -3,17 +3,17 @@
  *  * se modifica la importación dh-marvel ya que genera este error unable to resolve path to module
  */
 
-import React from 'react'
-import { type ComponentStory, type ComponentMeta } from '@storybook/react'
-import GeneralFooter from '../../../components/layouts/footer/general-footer.component'
+// import React from 'react'
+// import { type ComponentStory, type ComponentMeta } from '@storybook/react'
+// import GeneralFooter from '../../../components/layouts/footer/general-footer.component'
 
-export default {
-  title: 'Layout/Footer/GeneralFooter',
-  component: GeneralFooter,
-  argTypes: {}
-} as ComponentMeta<typeof GeneralFooter>
+// export default {
+//   title: 'Layout/Footer/GeneralFooter',
+//   component: GeneralFooter,
+//   argTypes: {}
+// } as ComponentMeta<typeof GeneralFooter>
 
-const Template: ComponentStory<typeof GeneralFooter> = (args: any) => <GeneralFooter {...args} />
+// const Template: ComponentStory<typeof GeneralFooter> = (args: any) => <GeneralFooter {...args} />
 
-export const Primary = Template.bind({})
-Primary.args = {}
+// export const Primary = Template.bind({})
+// Primary.args = {}
