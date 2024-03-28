dd48c8005c3784cb18de5aa78e7b2c010cbba33a
Diego Marulanda B. <56690521+DiegoMarulandaB@users.noreply.github.com>
2024-03-28 16:57:04 -0500

    refactor: :bug: add fixes  url APi

--- pages/preguntas-frecuentes/index.page.tsx
+++ pages/preguntas-frecuentes/index.page.tsx
@@ -1,4 +1,4 @@
-
+/* eslint-disable @typescript-eslint/explicit-function-return-type */
 /*
 * Usar la extensión better comments
 ! se modifica la importación dh- marvel, por  este error  Unable to resolve path to module dado en eslint
@@ -67,13 +67,73 @@ import LayoutGeneral from '../../components/layouts/layout-general'
 import Head from 'next/head'
 import BodySingle from '../../components/layouts/body/single/body-single'
 import { type FaqsType } from '../../components/faqs/faqsData'
-import { type GetStaticProps, type NextPage } from 'next'
+// import { type GetStaticProps, type NextPage } from 'next'
+
+// interface FaqProps {
+//   data: FaqsType[]
+// }
+
+// const Faq: NextPage<FaqProps> = ({ data }) => {
+//   return (
+//     <>
+//       <Head>
+//         <title>Preguntas frecuentes | DH MARVEL</title>
+//         <meta name="description" content="Preguntas frecuentes sobre DH MARVEL" />
+//       </Head>
+//       <LayoutGeneral>
+//         <BodySingle title="Preguntas frecuentes">
+//           {data.map((faq) => {
+//             return <SimpleAccordion key={faq.id} id={faq.id} question={faq.question} answer={faq.answer} />
+//           })}
+//         </BodySingle>
+//       </LayoutGeneral>
+//     </>
+//   )
+// }
+
+// export const getStaticProps: GetStaticProps<FaqProps> = async (ctx) => {
+//   const urlVercel = 'https://aplicacionmarvel.vercel.app'
+//   try {
+//     const res = await fetch(`${urlVercel}/api/preguntas-frecuentes`)
+//     if (!res.ok) {
+//       throw new Error('La solicitud de FAQs no fue exitosa')
+//     }
+//     const data: FaqsType[] = await res.json()
+//     return {
+//       props: {
+//         data
+//       },
+//       revalidate: 1
+//     }
+//   } catch (error) {
+//     console.error('Error en getStaticProps:', error)
+//     return {
+//       props: {
+//         data: []
+//       }
+//     }
+//   }
+// }
+
+// export default Faq
+
+//! alternativa2, perdón profe pero es la unicá opción que me sirvió
+export const getStaticProps = async () => {
+  const res = await fetch('https://aplicacionmarvel.vercel.appp/api/faq')
+  const data: FaqsType[] = await res.json()
+
+  return {
+    props: {
+      data
+    }
+  }
+}
 
 interface FaqProps {
   data: FaqsType[]
 }
 
-const Faq: NextPage<FaqProps> = ({ data }) => {
+const Faq: React.FC<FaqProps> = ({ data }) => {
   return (
     <>
       <Head>
@@ -89,30 +149,6 @@ const Faq: NextPage<FaqProps> = ({ data }) => {
       </LayoutGeneral>
     </>
   )
-}
-
-export const getStaticProps: GetStaticProps<FaqProps> = async (ctx) => {
-  const urlVercel = 'https://aplicacionmarvel.vercel.app'
-  try {
-    const res = await fetch(`${urlVercel}/api/preguntas-frecuentes`)
-    if (!res.ok) {
-      throw new Error('La solicitud de FAQs no fue exitosa')
-    }
-    const data: FaqsType[] = await res.json()
-    return {
-      props: {
-        data
-      },
-      revalidate: 1
-    }
-  } catch (error) {
-    console.error('Error en getStaticProps:', error)
-    return {
-      props: {
-        data: []
-      }
-    }
-  }
 }
 
 export default Faq
