c5cc24dee14342ddf5cf8804cfff9af11c2570ba
Diego Marulanda B. <56690521+DiegoMarulandaB@users.noreply.github.com>
2024-06-06 18:44:53 -0500

    refactor: :sparkles: add revalidate in index and fixes getFaqs

--- services/faqs/getFaqs.ts
+++ services/faqs/getFaqs.ts
@@ -1,15 +1,17 @@
+/* eslint-disable @typescript-eslint/explicit-function-return-type */
 // const API_URL = process.env.NEXT_PUBLIC_API_URL
 
-// export const getFaqs = async () => {
+export const getFaqs = async () => {
+    const response = await fetch('http://localhost:3000/api/preguntas-frecuentes')
+    // const response = await fetch(`${API_URL}/api/preguntas-frecuentes`);
+    const data = await response.json()
+    return data
+}
+
+// ! correciones mias con chat
+
+// export const getFaqs = async (): Promise<any> => {
 //   const response = await fetch('http://localhost:3000/api/preguntas-frecuentes')
 //   const data = await response.json()
 //   return data
 // }
-
-// ! correciones mias con chat
-
-export const getFaqs = async (): Promise<any> => {
-  const response = await fetch('http://localhost:3000/api/preguntas-frecuentes')
-  const data = await response.json()
-  return data
-}
